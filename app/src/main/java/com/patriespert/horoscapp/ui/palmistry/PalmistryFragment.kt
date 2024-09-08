714a1c3fa2f1bde1f29a332faeb88941a9b56611
Patricia Espert <patrimespert@outlook.com>
2024-09-08 20:42:10 +0200

    palmistry

--- app/src/main/java/com/patriespert/horoscapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/patriespert/horoscapp/ui/palmistry/PalmistryFragment.kt
@@ -1,14 +1,18 @@
 package com.patriespert.horoscapp.ui.palmistry
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
-import com.patriespert.horoscapp.Manifest
 import com.patriespert.horoscapp.databinding.FragmentPalmistryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -25,9 +29,9 @@ class PalmistryFragment : Fragment() {
     private val requestPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission()
     ) { isGranted ->
-        if(isGranted){
+        if (isGranted) {
             startCamera()
-        }else{
+        } else {
             Toast.makeText(
                 requireContext(),
                 "Acepta los permisos para poder disfrutar de una experiencia mágica",
@@ -36,31 +40,55 @@ class PalmistryFragment : Fragment() {
         }
     }
 
-    private fun startCamera() {
-
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        if (checkCameraPermission()) {
 
+        if (checkCameraPermission()) {
+            startCamera()
         } else {
             requestPermissionLauncher.launch(CAMERA_PERMISSION)
         }
     }
 
-    fun checkCameraPermission(): Boolean {
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try{
+                cameraProvider.unbindAll()
+
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            }catch (e:Exception){
+                Log.e("aris", "Algo petó ${e.message}")
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun checkCameraPermission(): Boolean {
         return PermissionChecker.checkSelfPermission(
-            requireContext(),
-            CAMERA_PERMISSION
+            requireContext(), CAMERA_PERMISSION
         ) == PermissionChecker.PERMISSION_GRANTED
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
         _binding = FragmentPalmistryBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
